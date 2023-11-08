762cac927f54487952fadfc75715d2ce5fcbf32b
kimyounghyeon <fourkimm@naver.com>
2023-11-08 10:32:21 +0900

    기초세팅

---
+++ src/app/confirm/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+        <h1>결재 현황 페이지
+          </h1>
+    </div>
+  )
+}
---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+<div>
+    <h1>나의 현황 페이지</h1>
+</div>
+  )
+}
