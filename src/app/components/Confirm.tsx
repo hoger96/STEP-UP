719bd50a25107a0c62754f1a476aa1f68d6534c1
kimyounghyeon <fourkimm@naver.com>
2023-11-16 10:17:13 +0900

    feat: 결재 상세 팝업 완성

--- src/app/components/Confirm.tsx
+++ src/app/components/Confirm.tsx
@@ -1,20 +1,39 @@
-import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from '@nextui-org/react'
-import React, { ReactNode } from 'react'
+import {
+  Button,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  useDisclosure,
+} from "@nextui-org/react";
+import React, { ReactNode } from "react";
 
 export default function CommonModal(props: {
-  title: ReactNode;   
+  title: ReactNode;
   contents: ReactNode;
-  size: "xs" | "sm" | "md" | "lg" | "xl" | "2xl" | "3xl" | "4xl" | "5xl" | "full" | undefined;
-  isOpen: boolean // 열림 flag
+  size:
+    | "xs"
+    | "sm"
+    | "md"
+    | "lg"
+    | "xl"
+    | "2xl"
+    | "3xl"
+    | "4xl"
+    | "5xl"
+    | "full"
+    | undefined;
+  isOpen: boolean; // 열림 flag
   useCustomBtn?: boolean; // 기본 버튼 형태를 사용하지 않을 때 true
   customButton?: ReactNode; // 설정하고 싶은 버튼의 형태를 받음
   confirmBtn?: string; // 확인 버튼 위치의 버튼 명 기본값 : 확인
   onConfirmBtn: (() => void) | undefined; // 확인 버튼의 동작을 설정
-  onClose: (() => void) | undefined
-}){
+  onClose: (() => void) | undefined;
+}) {
   return (
     <div>
-       <Modal 
+      <Modal
         isOpen={props.isOpen}
         onClose={props.onClose}
         size={props.size}
@@ -22,32 +41,35 @@ export default function CommonModal(props: {
       >
         <ModalContent>
           {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1">{props.title}</ModalHeader>
-              <ModalBody>
-                {props.contents}
-              </ModalBody>
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                {props.title}
+              </ModalHeader>
+              <ModalBody>{props.contents}</ModalBody>
               <ModalFooter>
-                {
-                  (() => {
-                    if(props.useCustomBtn) return <div>{props.customButton}</div>
-                    else return (
+                {(() => {
+                  if (props.useCustomBtn)
+                    return <div>{props.customButton}</div>;
+                  else
+                    return (
                       <div>
-                        <Button  variant="light" onPress={onClose}>
+                        <Button variant="light" onPress={onClose}>
                           취소
                         </Button>
-                        <Button className="bg-[#6f4ef2] shadow-lg shadow-indigo-500/20" onPress={props.onConfirmBtn}>
-                          {props.confirmBtn ? props.confirmBtn : '확인'}
+                        <Button
+                          className="bg-[#6f4ef2] shadow-lg shadow-indigo-500/20"
+                          onPress={props.onConfirmBtn}
+                        >
+                          {props.confirmBtn ? props.confirmBtn : "확인"}
                         </Button>
-                      </div>)
-                  })()
-                 }
+                      </div>
+                    );
+                })()}
               </ModalFooter>
             </>
           )}
         </ModalContent>
       </Modal>
     </div>
-  )
+  );
 }
-
--- src/app/confirm/_components/ConfirmContainer.tsx
+++ src/app/confirm/_components/ConfirmContainer.tsx
@@ -1,28 +1,99 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ConfirmTable from "./ConfirmTable";
 import { SearchBar } from "./SearchBar";
+import axios from "axios";
 
-interface ISearchParams {
-  searchType: string;
-  keyword: string;
-  approvalStatus: string;
-  startDate: string;
-  endDate: string;
+interface ISearchParams extends ICurrentPage {
+  searchType?: string;
+  keyword?: string;
+  approvalStatus?: string;
+  startDate?: string;
+  endDate?: string;
+}
+interface ICurrentPage {
+  currentPage?: number;
+}
+interface IManagementApproval {
+  rowNum: number;
+  approvalId: string;
+  userNm: string;
+  draftDt: string;
+  approvalReqDt: string;
+  approvalReqType: string;
+  approvalReqTypeNm: string;
+  approvalStus: string;
+  approvalStusNm: string;
+}
+
+interface IWrap<T> {
+  data: T[];
+  totalCount: number;
+  totalPage: number;
   currentPage: number;
-  limit: number;
 }
 
 export default function ConfirmContainer() {
-  const [searchData, setSearchData] = useState<ISearchParams | null>(null);
+  const [fetchDataResult, setFetchDataResult] =
+    useState<IWrap<IManagementApproval>>(); // 결재 현황 API 조회 결과
+  const now = new Date();
+  const oneMonthAgo = new Date(now);
+  oneMonthAgo.setMonth(now.getMonth() - 1); // 시작할 때만 동작
+  const [searchOption, setSearchOption] = useState<ISearchParams>({
+    searchType: "ALL",
+    keyword: "",
+    approvalStatus: "ALL",
+    startDate: oneMonthAgo.toISOString().split("T")[0],
+    endDate: now.toISOString().split("T")[0],
+    currentPage: 1,
+  });
   const handleSearch = (searchParams: ISearchParams) => {
-    setSearchData(searchParams);
+    setSearchOption(searchParams);
   };
+  const handleChange = (changePage: number) => {
+    setSearchOption({
+      ...searchOption,
+      currentPage: changePage,
+    });
+  };
+
+  const getApprovalListData = async (searchOption: ISearchParams) => {
+    try {
+      const result = await axios.get("/stepup/api/management/approval", {
+        params: {
+          searchType: searchOption.searchType,
+          keyword: searchOption.keyword,
+          approvalStatus: searchOption.approvalStatus,
+          startDate: searchOption.startDate,
+          endDate: searchOption.endDate,
+          currentPage: searchOption.currentPage,
+        },
+      });
+      return result.data.body;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+  const setApprovalListData = async () => {
+    const result = await getApprovalListData(searchOption);
+    setFetchDataResult(result);
+  };
+
+  useEffect(() => {
+    setApprovalListData();
+  }, []);
+
+  useEffect(() => {
+    setApprovalListData();
+  }, [searchOption]);
 
   return (
     <div>
       <SearchBar onSearch={handleSearch} />
-      <ConfirmTable searchData={searchData} />
+      <ConfirmTable
+        onChange={handleChange}
+        approvalDataList={fetchDataResult}
+      />
     </div>
   );
 }
