fbedac68ea0e006da8bd3b6b65e03683048556b9
kimyounghyeon <fourkimm@naver.com>
2023-11-21 10:12:51 +0900

    refactor: keyup 기능 추가

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,36 +1,44 @@
-import { Input } from '@nextui-org/react'
-import React, { ChangeEventHandler, ReactNode } from 'react'
+import { Input } from "@nextui-org/react";
+import React, {
+  ChangeEventHandler,
+  KeyboardEventHandler,
+  ReactNode,
+} from "react";
 
 export default function CommonInput(props: {
-    value: string | (readonly string[] & string) | undefined; 
-    type?: string | undefined; 
-    label?: boolean | React.ReactChild | React.ReactFragment | React.ReactPortal | null | undefined; 
-    labelPlacement?: "outside" | "outside-left" | "inside" | undefined; 
-    placeholder?: string | undefined; 
-    description?: ReactNode
-    isClearable?: boolean | undefined;
-    isRequired?: boolean | undefined;
-    isDisabled?: boolean | undefined;
-    isReadOnly?: boolean | undefined;
-    isInvalid?: boolean | undefined;
-    errorMessage?: ReactNode;
-    onValueChange?: ((value: string) => void) | undefined;
-    onClear?: () => any
+  value: string | (readonly string[] & string);
+  type?: string;
+  label?:
+    | boolean
+    | React.ReactChild
+    | React.ReactFragment
+    | React.ReactPortal
+    | null;
+  labelPlacement?: "outside" | "outside-left" | "inside";
+  placeholder?: string;
+  description?: ReactNode;
+  isClearable?: boolean;
+  isRequired?: boolean;
+  isDisabled?: boolean;
+  isReadOnly?: boolean;
+  isInvalid?: boolean;
+  errorMessage?: ReactNode;
+  onValueChange?: (value: string) => void;
+  onClear?: () => any;
 }) {
   return (
     <div>
-    <Input
-     classNames={{
-      label: "text-left font-semibold w-24",
-      input: [
-        "min-w-full w-64",
-        "placeholder:text-gray-400",
-      ]
-      }}
-        type={props.type ? props.type : 'text'}
+      <Input
+        classNames={{
+          label: "text-left font-semibold w-24",
+          input: ["min-w-full w-64", "placeholder:text-gray-400"],
+        }}
+        type={props.type ? props.type : "text"}
         value={props.value}
         label={props.label}
-        labelPlacement={props.labelPlacement ? props.labelPlacement : 'outside-left'}
+        labelPlacement={
+          props.labelPlacement ? props.labelPlacement : "outside-left"
+        }
         placeholder={props.placeholder}
         description={props.description}
         errorMessage={props.errorMessage}
@@ -40,8 +48,7 @@ export default function CommonInput(props: {
         isDisabled={props.isDisabled}
         isInvalid={props.isInvalid}
         onValueChange={props.onValueChange}
-    />
+      />
     </div>
-  )
+  );
 }
-
