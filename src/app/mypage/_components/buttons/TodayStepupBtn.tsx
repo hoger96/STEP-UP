93d416e5cf1df7d46a8c9ffe4e018d99062f36c5
kimyounghyeon <fourkimm@naver.com>
2023-11-22 14:45:58 +0900

    refactor: holdYn 으로 버튼 disabled하기

--- src/app/mypage/_components/buttons/TodayStepupBtn.tsx
+++ src/app/mypage/_components/buttons/TodayStepupBtn.tsx
@@ -1,76 +1,91 @@
-'use client'
+"use client";
 
-import CommonButton from '@/app/components/Buttons'
-import CommonModal from '@/app/components/Confirm'
-import React, { useState } from 'react'
-import CreateTodayStepupP from '../popup/CreateTodayStepupP'
-import axios, { AxiosError } from 'axios'
-import { ToastContainer, toast } from 'react-toastify'
+import CommonButton from "@/app/components/Buttons";
+import CommonModal from "@/app/components/Confirm";
+import React, { useState } from "react";
+import CreateTodayStepupP from "../popup/CreateTodayStepupP";
+import axios, { AxiosError } from "axios";
+import { ToastContainer, toast } from "react-toastify";
 
 interface IProps {
-  requestId: string
-  onRefreshTable: () => void
+  requestId: string;
+  onRefreshTable: () => void;
 }
 
 export default function TodayStepupBtn(props: IProps) {
+  const userId = sessionStorage.getItem("loginUserId");
 
-  const userId = sessionStorage.getItem('loginUserId')
-
-  const [isOpen, setIsOpen] = useState(false)
-  const [todayDate, setTodayDate] = useState(new Date().toISOString().split('T')[0]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [todayDate, setTodayDate] = useState(
+    new Date().toISOString().split("T")[0]
+  );
   const [startTm, setStartTm] = useState<Date>(new Date());
   const [endTm, setEndTm] = useState<Date>(new Date());
-  const [file, setFile] = useState<any>('');
+  const [file, setFile] = useState<any>("");
+  const holdYn = sessionStorage.getItem("holdYn");
 
   const handelOpenCreateTodayStepupPopup = () => {
-    setIsOpen(true)
-  }
+    setIsOpen(true);
+  };
 
-  const setParams = async (startTm: Date, endTm: Date, userId: string, file: File[]) => {
-    const formData = new FormData()
+  const setParams = async (
+    startTm: Date,
+    endTm: Date,
+    userId: string,
+    file: File[]
+  ) => {
+    const formData = new FormData();
 
-    const options: Intl.DateTimeFormatOptions = { hour: "numeric", minute: "numeric", hour12: false }
+    const options: Intl.DateTimeFormatOptions = {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: false,
+    };
 
-    const startTime = new Date(startTm).toLocaleTimeString("ko-KR", options)
-    const endTime = new Date(endTm).toLocaleTimeString("ko-KR", options)
+    const startTime = new Date(startTm).toLocaleTimeString("ko-KR", options);
+    const endTime = new Date(endTm).toLocaleTimeString("ko-KR", options);
 
-    formData.append('startTm', startTime)
-    formData.append('endTm', endTime)
-    formData.append('userId', userId)
+    formData.append("startTm", startTime);
+    formData.append("endTm", endTime);
+    formData.append("userId", userId);
     if (file[0]) {
-      formData.append("file", new Blob([await file[0].arrayBuffer()], { type: file[0].type }), file[0].name);
+      formData.append(
+        "file",
+        new Blob([await file[0].arrayBuffer()], { type: file[0].type }),
+        file[0].name
+      );
     }
-    return formData
-  }
+    return formData;
+  };
 
   const createTodayStepup = async (formData: FormData) => {
     try {
-      await axios.post('/stepup/api/user/today-exercise', formData, {
+      await axios.post("/stepup/api/user/today-exercise", formData, {
         headers: {
-          'Content-Type': 'multipart/form-data',
-        }
-      })
+          "Content-Type": "multipart/form-data",
+        },
+      });
 
-      return true
+      return true;
     } catch (e) {
       if (e instanceof AxiosError) {
-        toast.error(e?.response?.data.message)
-        return false
+        toast.error(e?.response?.data.message);
+        return false;
       }
     }
-  }
+  };
 
   const onConfirmBtn = async () => {
     if (!userId) {
-      return
+      return;
     }
-    const params = await setParams(startTm, endTm, userId, file)
+    const params = await setParams(startTm, endTm, userId, file);
     if (params) {
-      const isSuccess = await createTodayStepup(params)
+      const isSuccess = await createTodayStepup(params);
       if (isSuccess) {
-        setIsOpen(false)
+        setIsOpen(false);
         props.onRefreshTable();
-        toast.success('축하합니다! 오늘의 운동기록을 기록하셨군요 :)')
+        toast.success("축하합니다! 오늘의 운동기록을 기록하셨군요 :)");
       } else {
         // toast.error('오늘의 운동기록을 추가하지 못했어요')
       }
@@ -78,27 +93,27 @@ export default function TodayStepupBtn(props: IProps) {
   };
 
   const onClose = () => {
-    setStartTm(new Date())
-    setEndTm(new Date())
-    setFile([])
-    setIsOpen(false)
-  }
+    setStartTm(new Date());
+    setEndTm(new Date());
+    setFile([]);
+    setIsOpen(false);
+  };
 
   return (
     <div>
       <CommonButton
-        label={'오늘의 스텝업 기록하기'}
-        size={'sm'}
-        radius={'sm'}
-        color={'default'}
-        variant={'flat'}
-        isDisabled={userId !== props.requestId}
+        label={"오늘의 스텝업 기록하기"}
+        size={"sm"}
+        radius={"sm"}
+        color={"default"}
+        variant={"flat"}
+        isDisabled={userId !== props.requestId || holdYn === "Y"}
         onClick={handelOpenCreateTodayStepupPopup}
       />
       <ToastContainer position="top-right" autoClose={2000} />
       <div>
         <CommonModal
-          title={'오늘의 스텝업 기록하기'}
+          title={"오늘의 스텝업 기록하기"}
           contents={
             <CreateTodayStepupP
               todayDate={todayDate}
@@ -109,15 +124,14 @@ export default function TodayStepupBtn(props: IProps) {
               setStartTm={setStartTm}
               setEndTm={setEndTm}
               setFile={setFile}
-            />}
-          size={'2xl'}
+            />
+          }
+          size={"2xl"}
           isOpen={isOpen}
           onConfirmBtn={onConfirmBtn}
           onClose={onClose}
         />
       </div>
     </div>
-  )
+  );
 }
-
-
